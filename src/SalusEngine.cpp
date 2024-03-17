5ee6bde061cbdddd76eceec177b4754c6b455ec3
Shashank P <shashankp2832@gmail.com>
2024-03-17 21:21:01 +0530

    Added 'addPieceOfInformation' method to Engine

--- src/SalusEngine.cpp
+++ src/SalusEngine.cpp
@@ -1,5 +1,6 @@
 #include <SalusEngine.h>
 #include <RuleCollection.h>
+#include <PieceOfInformation.h>
 #include <HierarchyStructure.h>
 #include <iostream>
 #include <Logging.h>
@@ -11,7 +12,7 @@ SalusEngine::SalusEngine(std::string topUser)
     informationBank = new std::unordered_map<std::string, PieceOfInformation *>();
     ruleCollections = new std::unordered_map<std::string, RuleCollection *>();
     hierarchies = new std::unordered_map<std::string, HierarchyStructure *>();
-    
+
     // Initializing Engine
     Logging::init("salus.log", 50, true);
     HierarchyStructure *userHierarchy = new HierarchyStructure("User", nullptr);
@@ -27,6 +28,36 @@ void SalusEngine::addPieceOfInformation(std::string id, std::string owner,
 {
     if (informationBank->find(id) != informationBank->end())
     {
+        Logging::log(LogLevel::ERROR, "Piece of information " + id + " already exists");
         throw std::runtime_error("Piece of information already exists");
     }
+    HierarchyNode *ownerNode = hierarchies->at("User")->getMember(owner);
+    if (ownerNode == nullptr)
+    {
+        Logging::log(LogLevel::ERROR, "Owner " + owner + " does not exist");
+        throw std::runtime_error("Owner does not exist");
+    }
+    PieceOfInformation *newPiece = new PieceOfInformation(id, ownerNode);
+    for (auto it = readAccessList.begin(); it != readAccessList.end(); it++)
+    {
+        RuleCollection *ruleCollection = ruleCollections->at(*it);
+        if (ruleCollection == nullptr)
+        {
+            Logging::log(LogLevel::ERROR, "Read Rule Collection " + *it + " does not exist");
+            throw std::runtime_error("Read access rule collection does not exist");
+        }
+        newPiece->addReadAccessRule(*it, ruleCollection);
+    }
+    for (auto it = writeAccessList.begin(); it != writeAccessList.end(); it++)
+    {
+        RuleCollection *ruleCollection = ruleCollections->at(*it);
+        if (ruleCollection == nullptr)
+        {
+            Logging::log(LogLevel::ERROR, "Write Rule Collection " + *it + " does not exist");
+            throw std::runtime_error("Write access rule collection does not exist");
+        }
+        newPiece->addWriteAccessRule(*it, ruleCollection);
+    }
+    informationBank->insert({id, newPiece});
+    Logging::log(LogLevel::INFO, "Piece of information " + id + " added successfully");
 }
